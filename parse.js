a4e1b10bf0f35f832fea4fd8035edf1228ef2c4a
m4-z3 <m4-z3@users.noreply.github.com>
2020-01-14 19:01:11 -0600

    adjusted ordercheck to exclude logical operators in front of ( from returning false

--- parse.js
+++ parse.js
@@ -2,7 +2,8 @@ class Parse {
     constructor(tokens) {
         this.tokenList = new Array();
         if (tokens.length != 0) {
-            this.tokenList = tokens; // expect a array of tokens from the lexer
+            // expect a array of tokens from the lexer
+            this.tokenList = tokens; 
         }
         this.parsedObjects = {};
     }
@@ -31,38 +32,47 @@ class Parse {
         this.tokenList.forEach(token => {
             // check parens
             if (token.type === tokenType.startParentheses) { // checking for start parentheses
+                // checking for start parentheses
                 parensChecker.push(token);
             }
-            else if (token.type === tokenType.endParentheses) { // checking for end parentheses and whether it's placed correctly
+            else if (token.type === tokenType.endParentheses) { 
+                // checking for end parentheses and whether it's placed correctly
                 if (parensChecker.length == 0) {
                     return false;
                 }
                 parensChecker.pop();
             }
             // check propositions
-            else if (token.type === tokenType.proposition && isPreviousProp) { // checking to make sure all propositions have logical operators in between them
+            else if (token.type === tokenType.proposition && isPreviousProp) {
+                 // checking to make sure all propositions have logical operators in between them
                 return false;
             }
             // check logical operators
             else if ((token.type === tokenType.and || token.type === tokenType.or || token.type === tokenType.conditional || token.type === tokenType.biconditional) && !isPreviousProp) {
+                // checking to make sure logical and, or, conditional, and biconditional operators have a preposition before
                 return false;
             }
-            else if (isPreviousOperator && token.type !== tokenType.proposition) {
+            else if (isPreviousOperator && (token.type !== tokenType.proposition || token.type != tokenType.startParentheses)) {
+                // checking to make sure operators have a proposition or start parentheses afterwards
                 return false;
             }
 
             // setting booleans to correct value
-            if (token.type !== tokenType.proposition && isPreviousProp) { // reset isPreviousProp to false when token is not proposition
+            if (token.type !== tokenType.proposition && isPreviousProp) { 
+                // reset isPreviousProp to false when token is not proposition
                 isPreviousProp = false;
             }
-            else if (token.type === tokenType.proposition && !isPreviousProp) { // marking the current token (which will become the previous token in the next loop) as true for isPreviousProp
+            else if (token.type === tokenType.proposition && !isPreviousProp) { 
+                // marking the current token (which will become the previous token in the next loop) as true for isPreviousProp
                 isPreviousProp = true;
             }
 
-            if (token.type === tokenType.and || token.type === tokenType.or || token.type === tokenType.not || token.type === tokenType.conditional || token.type === tokenType.biconditional) { // marks previous operator as true if there is an and, or, not
+            if (token.type === tokenType.and || token.type === tokenType.or || token.type === tokenType.not || token.type === tokenType.conditional || token.type === tokenType.biconditional) {
+                 // marks previous operator as true if there is an and, or, not
                 isPreviousOperator = true;
             }
-            else if (isPreviousOperator) { // if isPreviousOperator is set to true, then set to false
+            else if (isPreviousOperator) { 
+                // if isPreviousOperator is set to true, then set to false
                 isPreviousOperator = false;
             }
         });
@@ -78,6 +88,7 @@ class Parse {
         // assuming parsedObjects is empty
         let numOfPropositions = 0;
 
+        // first finds the prepositions
         this.tokenList.forEach(t => {
             if (t.type == tokenType.proposition) {
                 this.parsedObjects[t.value] = [];
@@ -85,6 +96,7 @@ class Parse {
             }
         });
 
+        // and then gives each preposition true and false values so that every combination of true false is created
         let addTrue = true;
         let power = numOfPropositions - 1;
         let numPerGroup = Math.pow(2, power);
@@ -98,6 +110,7 @@ class Parse {
                 else {
                     this.parsedObjects[Object.keys(this.parsedObjects)[i]].push(false);
                 }
+                // used to alternate pushing true or false
                 if (currentGroupTot == numPerGroup) {
                     currentGroupTot = 0;
                     addTrue = !addTrue;
@@ -106,7 +119,7 @@ class Parse {
             // making sure that all necessary variables are set to initial state
             addTrue = true;
             currentGroupTot = 0;
-            // adjusting number of bools needed before switchin from true to false and vice versa
+            // adjusting number of bools needed before switching from true to false and vice versa
             power--;
             numPerGroup = Math.pow(2, power);
         }
